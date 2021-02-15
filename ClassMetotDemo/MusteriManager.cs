f2d3cdb8507c7c0ba93d3e1c5c348be33728e99b
BT <BT@DESKTOP-G7MDNAF>
2021-02-15 18:33:53 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri m)
+        {
+            Console.WriteLine("Müşteri sisteme eklendi... {0}", m.AdiSoyadi);
+        }
+
+        public void List(params Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Id : " + musteri.Id);
+                Console.WriteLine("Müşteri Adı Soyadı : " + musteri.AdiSoyadi);
+                Console.WriteLine("Müşterinin yaşadığı şehir : " + musteri.Sehir);
+            }
+        }
+
+        public void Delete(Musteri m)
+        {
+            Console.WriteLine("Müşteri sistemden silindi... {0}", m.AdiSoyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ("ClassMethotDemo");
+
+            Musteri m1 = new Musteri()
+            {
+                Id = 9000001,
+                AdiSoyadi = "Zehra Deneyici",
+                Sehir = "İstanbul"
+            };
+
+            Musteri m2 = new Musteri()
+            {
+                Id = 9000002,
+                AdiSoyadi = "Caner Deneyici",
+                Sehir = "Adana"
+            };
+
+            Musteri m3 = new Musteri()
+            {
+                Id = 9000003,
+                AdiSoyadi = "Ben Bilmem",
+                Sehir = "Van"
+            };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            //Ekle
+            musteriManager.Add(m1);
+            musteriManager.Add(m2);
+            musteriManager.Add(m3);
+
+            //Listele
+            musteriManager.List(m1, m2, m3);
+
+            //Sil
+            musteriManager.Delete(m1);
+            musteriManager.Delete(m2);
+            musteriManager.Delete(m3);
+
+        }
+    }
+}
